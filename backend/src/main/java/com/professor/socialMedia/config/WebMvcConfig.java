3e363c10a4675c2a6bbd6dd1028ad40560284048
Vipul Agarwal <vipulagarwal8055@gmail.com>
2026-02-24 23:13:54 +0530

    final Commit

--- backend/src/main/java/com/professor/socialMedia/config/WebMvcConfig.java
+++ backend/src/main/java/com/professor/socialMedia/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Frontend URL
+                .allowedOrigins("http://localhost:3000", "https://social-media-ecom.vercel.app") // Frontend URLs
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
