56396d4f8efaa29ee14408cf6109cc5065540650
Vipul Agarwal <vipulagarwal8055@gmail.com>
2026-03-08 15:11:52 +0530

    Remove frontend folder from repository

--- frontend/test-upload.js
+++
@@ -1,4 +0,0 @@
-<div className="relative aspect-square md:aspect-[4/5] w-full overflow-hidden rounded-2xl bg-[#e8e8e1]">
-
-
-<div className="relative aspect-[4/5] w-full overflow-hidden rounded-2xl bg-[#e8e8e1]">
